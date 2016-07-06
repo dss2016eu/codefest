c4c741f51e937afb9e6046809337280543414f1d
adamcsvarga <adamcs.varga@gmail.com>
2016-07-06 11:38:00 +0200

    sentence splitting

---
+++ zureTTS/split_sents.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  6 11:29:23 2016
+
+Splits text into sentences.
+
+Usage: python split_sents.py <input> <output>
+
+@author: vurga
+"""
+
+import sys, re 
+from nltk import tokenize
+
+with open(sys.argv[1], 'r') as target:
+    text = target.read()
+with open(sys.argv[2], 'w'):
+    pass
+    
+text = re.sub('"', '', text)
+sents = tokenize.sent_tokenize(text)
+
+for sent in sents:
+    with open(sys.argv[2], 'a+') as target:
+        target.write(sent + ' \n')
